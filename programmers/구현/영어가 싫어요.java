cacdddf047c703a60ea56b87c3e6f756aba1e977
Jiwon <rkdwldnjs878@gmail.com>
2024-06-29 23:12:25 +0900

    Create 영어가 싫어요.java

---
+++ programmers/구현/영어가 싫어요.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        
+        String number = numbers.replace("zero", "0")
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+        
+        answer = Long.parseLong(number);
+        
+        
+        return answer;
+    }
+}
